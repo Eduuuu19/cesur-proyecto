01df5ea83767f1e23ed45c64f2fa890b2b4ba2f5
Eduuuu19 <eduas1927@gmail.com>
2026-01-25 17:15:15 +0100

    Creo carpeta de documentacion y diario

---
+++ backend/src/main/java/com/konta/backend/KontaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.konta.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontaBackendApplication.class, args);
+	}
+
+}
